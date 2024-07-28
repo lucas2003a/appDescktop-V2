172b9ece54eaa4adf8643ecdf15b26676114a39d
lucas2003a <lucasatuncar1@gmail.com>
2024-07-28 18:35:34 -0500

    SearchInArray

---
+++ FomrSearchInArray/FormSearchInArray.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace appDescktop_V2.FomrSearchInArray
+{
+    partial class FormSearchInArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LiDataArray = new ListBox();
+            TxtDataArray = new TextBox();
+            BtnSearch = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // LiDataArray
+            // 
+            LiDataArray.FormattingEnabled = true;
+            LiDataArray.ItemHeight = 15;
+            LiDataArray.Location = new Point(36, 168);
+            LiDataArray.Name = "LiDataArray";
+            LiDataArray.Size = new Size(168, 94);
+            LiDataArray.TabIndex = 0;
+            // 
+            // TxtDataArray
+            // 
+            TxtDataArray.Location = new Point(71, 74);
+            TxtDataArray.Name = "TxtDataArray";
+            TxtDataArray.Size = new Size(100, 23);
+            TxtDataArray.TabIndex = 1;
+            // 
+            // BtnSearch
+            // 
+            BtnSearch.Location = new Point(80, 103);
+            BtnSearch.Name = "BtnSearch";
+            BtnSearch.Size = new Size(75, 23);
+            BtnSearch.TabIndex = 2;
+            BtnSearch.Text = "Buscar";
+            BtnSearch.UseVisualStyleBackColor = true;
+            BtnSearch.Click += BtnSearch_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(80, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Dato a buscar :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Datos del array :";
+            // 
+            // FormSearchInArray
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(246, 294);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(BtnSearch);
+            Controls.Add(TxtDataArray);
+            Controls.Add(LiDataArray);
+            Name = "FormSearchInArray";
+            Text = "Buscar datos - array";
+            Load += FormSearchInArray_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox LiDataArray;
+        private TextBox TxtDataArray;
+        private Button BtnSearch;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ FomrSearchInArray/FormSearchInArray.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appDescktop_V2.FomrSearchInArray
+{
+    public partial class FormSearchInArray : Form
+    {
+        public string[] Names { get; set; }
+        public FormSearchInArray()
+        {
+            this.Names = new string[]
+            {
+                "Lu", "Ana", "Luis", "Marta", "Carlos", "Sofia", "Javier", "Isabel",
+                "Pedro", "Laura", "Alejandro", "Carmen", "Antonio", "Paula", "Jorge",
+                "María", "Francisco", "Elena", "David", "Silvia"
+            };
+            InitializeComponent();
+        }
+
+        private void FormSearchInArray_Load(object sender, EventArgs e)
+        {
+            RenderArray(this.Names);
+        }
+
+        
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            string name = TxtDataArray.Text;
+            ValidateTextName(name);
+        }
+
+        /**
+         *Renderiza los datos del array en el listBox
+         */
+        private void RenderArray(string[] array)
+        {
+            LiDataArray.Items.Clear();
+
+            foreach (string name in array)
+            {
+                LiDataArray.Items.Add(name);
+            }
+        }
+
+        /**
+         * Valida el nombre ingresado desde el TextBox
+         */
+        private void ValidateTextName(string nameInput)
+        {
+            try
+            {
+                string name = nameInput;
+                SearchNameInArray(name);
+            }
+            catch (Exception ex) { 
+                ShowMessage(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /*
+         *Busca el nombre en el array
+         */
+        private void SearchNameInArray(string name)
+        {
+            string[] array = this.Names;
+            bool isFind = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (name.Equals(array[i]))
+                {
+                    isFind = true;
+                    break;
+                }
+
+            }
+
+            if (isFind) 
+                ShowMessage($"El nombre {name} ha sido encontrado", "Nombre encontrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                ShowMessage($"El nombre {name} no ha sido encontrado", "Nombre no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /*
+         *Muestra un mensaje personalizado
+         */
+        private void ShowMessage(string message, string title, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+            MessageBox.Show(message, title, buttons, icon);
+        }
+    }
+}
