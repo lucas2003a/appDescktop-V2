f90433161565db5476484b753a07776591b25525
lucas2003a <lucasatuncar1@gmail.com>
2024-07-28 23:40:32 -0500

    SearchInArray

--- FomrSearchInArray/FormSearchInArray.Designer.cs
+++ FomrSearchInArray/FormSearchInArray.Designer.cs
@@ -33,30 +33,36 @@
             BtnSearch = new Button();
             label1 = new Label();
             label2 = new Label();
+            BtnArrayNames = new Button();
+            BtnArrayAges = new Button();
+            GrpArrays = new GroupBox();
+            GrpSearchArray = new GroupBox();
+            GrpArrays.SuspendLayout();
+            GrpSearchArray.SuspendLayout();
             SuspendLayout();
             // 
             // LiDataArray
             // 
             LiDataArray.FormattingEnabled = true;
             LiDataArray.ItemHeight = 15;
-            LiDataArray.Location = new Point(36, 168);
+            LiDataArray.Location = new Point(36, 235);
             LiDataArray.Name = "LiDataArray";
             LiDataArray.Size = new Size(168, 94);
             LiDataArray.TabIndex = 0;
             // 
             // TxtDataArray
             // 
-            TxtDataArray.Location = new Point(71, 74);
+            TxtDataArray.Location = new Point(47, 37);
             TxtDataArray.Name = "TxtDataArray";
             TxtDataArray.Size = new Size(100, 23);
-            TxtDataArray.TabIndex = 1;
+            TxtDataArray.TabIndex = 3;
             // 
             // BtnSearch
             // 
-            BtnSearch.Location = new Point(80, 103);
+            BtnSearch.Location = new Point(56, 66);
             BtnSearch.Name = "BtnSearch";
             BtnSearch.Size = new Size(75, 23);
-            BtnSearch.TabIndex = 2;
+            BtnSearch.TabIndex = 4;
             BtnSearch.Text = "Buscar";
             BtnSearch.UseVisualStyleBackColor = true;
             BtnSearch.Click += BtnSearch_Click;
@@ -64,7 +70,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(80, 56);
+            label1.Location = new Point(56, 19);
             label1.Name = "label1";
             label1.Size = new Size(85, 15);
             label1.TabIndex = 3;
@@ -73,25 +79,70 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(36, 150);
+            label2.Location = new Point(36, 217);
             label2.Name = "label2";
             label2.Size = new Size(91, 15);
             label2.TabIndex = 3;
             label2.Text = "Datos del array :";
             // 
+            // BtnArrayNames
+            // 
+            BtnArrayNames.Location = new Point(15, 33);
+            BtnArrayNames.Name = "BtnArrayNames";
+            BtnArrayNames.Size = new Size(75, 23);
+            BtnArrayNames.TabIndex = 1;
+            BtnArrayNames.Text = "Nombres";
+            BtnArrayNames.UseVisualStyleBackColor = true;
+            BtnArrayNames.Click += BtnArrayNames_Click;
+            // 
+            // BtnArrayAges
+            // 
+            BtnArrayAges.Location = new Point(96, 33);
+            BtnArrayAges.Name = "BtnArrayAges";
+            BtnArrayAges.Size = new Size(75, 23);
+            BtnArrayAges.TabIndex = 2;
+            BtnArrayAges.Text = "Edades";
+            BtnArrayAges.UseVisualStyleBackColor = true;
+            BtnArrayAges.Click += BtnArrayAges_Click;
+            // 
+            // GrpArrays
+            // 
+            GrpArrays.Controls.Add(BtnArrayNames);
+            GrpArrays.Controls.Add(BtnArrayAges);
+            GrpArrays.Location = new Point(21, 12);
+            GrpArrays.Name = "GrpArrays";
+            GrpArrays.Size = new Size(200, 75);
+            GrpArrays.TabIndex = 6;
+            GrpArrays.TabStop = false;
+            GrpArrays.Text = "Arrays";
+            // 
+            // GrpSearchArray
+            // 
+            GrpSearchArray.Controls.Add(label1);
+            GrpSearchArray.Controls.Add(TxtDataArray);
+            GrpSearchArray.Controls.Add(BtnSearch);
+            GrpSearchArray.Location = new Point(21, 106);
+            GrpSearchArray.Name = "GrpSearchArray";
+            GrpSearchArray.Size = new Size(200, 97);
+            GrpSearchArray.TabIndex = 6;
+            GrpSearchArray.TabStop = false;
+            GrpSearchArray.Text = "Busqueda de arrays";
+            // 
             // FormSearchInArray
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(246, 294);
+            ClientSize = new Size(246, 347);
+            Controls.Add(GrpSearchArray);
+            Controls.Add(GrpArrays);
             Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(BtnSearch);
-            Controls.Add(TxtDataArray);
             Controls.Add(LiDataArray);
             Name = "FormSearchInArray";
             Text = "Buscar datos - array";
             Load += FormSearchInArray_Load;
+            GrpArrays.ResumeLayout(false);
+            GrpSearchArray.ResumeLayout(false);
+            GrpSearchArray.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -103,5 +154,9 @@
         private Button BtnSearch;
         private Label label1;
         private Label label2;
+        private Button BtnArrayNames;
+        private Button BtnArrayAges;
+        private GroupBox GrpArrays;
+        private GroupBox GrpSearchArray;
     }
 }
--- FomrSearchInArray/FormSearchInArray.cs
+++ FomrSearchInArray/FormSearchInArray.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace appDescktop_V2.FomrSearchInArray
 {
     public partial class FormSearchInArray : Form
     {
-        public string[] Names { get; set; }
+        private string[] Names { get; set; }
+        private int[] Ages { get; set; }
+
+        private bool IsNumber { get; set; }
         public FormSearchInArray()
         {
             this.Names = new string[]
@@ -21,25 +25,54 @@ namespace appDescktop_V2.FomrSearchInArray
                 "Pedro", "Laura", "Alejandro", "Carmen", "Antonio", "Paula", "Jorge",
                 "María", "Francisco", "Elena", "David", "Silvia"
             };
+
+            this.Ages = new int[]
+            {
+                1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                21,32, 53, 64, 85, 16, 27, 88, 79,
+                10, 11
+            };
             InitializeComponent();
         }
 
         private void FormSearchInArray_Load(object sender, EventArgs e)
         {
-            RenderArray(this.Names);
+            
         }
 
-        
+
 
         private void BtnSearch_Click(object sender, EventArgs e)
         {
-            string name = TxtDataArray.Text;
-            ValidateTextName(name);
+            if (this.IsNumber)
+            {
+                int age = int.Parse(TxtDataArray.Text);
+                ValidateTextName(age);
+            }
+            else
+            {
+
+                string name = TxtDataArray.Text;
+                ValidateTextName(name);
+            }
         }
 
-        /**
-         *Renderiza los datos del array en el listBox
-         */
+        private void BtnArrayNames_Click(object sender, EventArgs e)
+        {
+            this.IsNumber = false;
+            RenderArray(this.Names);
+        }
+
+        private void BtnArrayAges_Click(object sender, EventArgs e)
+        {
+            this.IsNumber = true;
+            RenderArray(this.Ages);
+        }
+
+        /// <summary>
+        /// Renderiza los datos del array en el ListBox
+        /// </summary>
+        /// <param name="array"></param>
         private void RenderArray(string[] array)
         {
             LiDataArray.Items.Clear();
@@ -49,33 +82,69 @@ namespace appDescktop_V2.FomrSearchInArray
                 LiDataArray.Items.Add(name);
             }
         }
+        
+        /// <summary>
+        /// Renderiza los datos del array en el ListBox
+        /// </summary>
+        /// <param name="array"></param>
+        private void RenderArray(int[] array)
+        {
+            LiDataArray.Items.Clear();
 
-        /**
-         * Valida el nombre ingresado desde el TextBox
-         */
-        private void ValidateTextName(string nameInput)
+            foreach (int age in array)
+            {
+                string AgeString = age.ToString();
+                LiDataArray.Items.Add(AgeString);
+            }
+        }
+
+        /// <summary>
+        /// Valida el nombre ingresado desde el TextBox
+        /// </summary>
+        /// <param name="nameInput"></param>
+        private void ValidateTextName(string NameInput)
         {
             try
             {
-                string name = nameInput;
+                string name = NameInput;
                 SearchNameInArray(name);
             }
-            catch (Exception ex) { 
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        /// <summary>
+        /// Valida la edad ingresada desde el TextBox
+        /// </summary>
+        /// <param name="AgeInput"></param>
+        private void ValidateTextName(int AgeInput)
+        {
+            try
+            {
+                int age = AgeInput;
+                SearchNameInArray(age);
+            }
+            catch (Exception ex)
+            {
                 ShowMessage(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        /*
-         *Busca el nombre en el array
-         */
-        private void SearchNameInArray(string name)
+        /// <summary>
+        /// Busca el nombre en el array
+        /// </summary>
+        /// <param name="name"></param>
+        private void SearchNameInArray(string value)
         {
             string[] array = this.Names;
+
             bool isFind = false;
 
             for (int i = 0; i < array.Length; i++)
             {
-                if (name.Equals(array[i]))
+                if (value.Equals(array[i]))
                 {
                     isFind = true;
                     break;
@@ -83,18 +152,50 @@ namespace appDescktop_V2.FomrSearchInArray
 
             }
 
-            if (isFind) 
-                ShowMessage($"El nombre {name} ha sido encontrado", "Nombre encontrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (isFind)
+                ShowMessage($"{value} ha sido encontrado", "Dato encontrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
             else
-                ShowMessage($"El nombre {name} no ha sido encontrado", "Nombre no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ShowMessage($"{value} no ha sido encontrado", "Dato no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        
+        /// <summary>
+        /// Busca la edad en el array
+        /// </summary>
+        /// <param name="value"></param>
+        private void SearchNameInArray(int value)
+        {
+            int[] array = this.Ages;
+
+            bool isFind = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (value.Equals(array[i]))
+                {
+                    isFind = true;
+                    break;
+                }
+
+            }
+
+            if (isFind)
+                ShowMessage($"{value} ha sido encontrado", "Dato encontrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                ShowMessage($"{value} no ha sido encontrado", "Dato no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        /*
-         *Muestra un mensaje personalizado
-         */
+        /// <summary>
+        /// Muestra un mensaje personalizado
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="title"></param>
+        /// <param name="buttons"></param>
+        /// <param name="icon"></param>
         private void ShowMessage(string message, string title, MessageBoxButtons buttons, MessageBoxIcon icon)
         {
             MessageBox.Show(message, title, buttons, icon);
         }
+
+        
     }
 }
